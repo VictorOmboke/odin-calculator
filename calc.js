9c3821d01a7d67affcb80d2cceddb5cd5cb7cc1f
Victor Omboke <victoromboke@gmail.com>
2023-01-02 14:50:54 -0800

    Create simple math functions and fix typo on index.HTML

---
+++ calc.js
@@ -0,0 +1,19 @@
+function add(x, y) {
+    return x + y;
+}
+console.log(add(2, 5));
+
+function subtract(x, y) {
+    return x - y;
+}
+console.log(subtract(10, 2));
+
+function multiply(x, y) {
+    return x * y;
+}
+console.log(multiply(2, 10));
+
+function divide(x, y) {
+    return x / y;
+}
+console.log(divide(100, 2));
